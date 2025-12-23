8d7f611b4cf6988a51e6c60d6dc79c0ae844465e
suzuki toshiya <mpsuzuki@hiroshima-u.ac.jp>
2025-12-23 17:35:21 +0900

    Add CStringHash.hpp to replace deprecated ext/hash_map & ext/hash_set in GNU C++03

---
+++ src/ld/CStringHash.hpp
@@ -0,0 +1,19 @@
+#ifndef LD_CSTRINGHASH_HPP
+#define LD_CSTRINGHASH_HPP
+
+#include <cstring>
+#include <unordered_map>
+
+// A tiny & simple function to calculate a hash value from a string.
+struct CStringHash {
+    size_t operator()(const char* s) const noexcept {
+        size_t h = 0;
+        while (*s) {
+            h = h * 131 + static_cast<unsigned char>(*s++);
+        }
+        return h;
+    }
+};
+
+#endif
+
